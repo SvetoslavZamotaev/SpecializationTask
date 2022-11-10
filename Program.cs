72b88c14bfc6cf366dcbc8e52b680a6a8db85472
Svetoslav Zamotaev <zamatory1990@gmail.com>
2022-11-10 09:25:04 +0300

    Создал метод записи нужных элементов в результирующий массив

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
 ﻿string[] startArr = { "Doom", "GTA", "Hunt", "^^)", "<3", "OpenGL", "D2", "Gothic", "1984" };
 
-int CountThreeLetter(string[] arr, int len)
+int CountThreeLetter(string[] arr, int requiredLen)
 {
-    int result = 0;
+    int count = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length <= len)
+        if (arr[i].Length <= requiredLen)
         {
-            result++;
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] RequiredArray(string[] arr, int count, int requiredLen)
+{
+    string[] result = new string[count];
+    int indexResult = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= requiredLen)
+        {
+            result[indexResult] = arr[i];
+            indexResult++;
         }
     }
     return result;
 }
 
-
+Console.WriteLine(string.Join(' ', RequiredArray(startArr, CountThreeLetter(startArr, 3), 3)));
