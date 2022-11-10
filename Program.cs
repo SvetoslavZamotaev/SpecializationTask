9dcd669090e8eff0eb84083c843e65e65e3a3f01
Svetoslav Zamotaev <zamatory1990@gmail.com>
2022-11-10 09:08:06 +0300

    Создал метод подсчета элементов нужной длины

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿string[] startArr = { "Doom", "GTA", "Hunt", "^^)", "<3", "OpenGL", "D2", "Gothic", "1984" };
+
+int CountThreeLetter(string[] arr, int len)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= len)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
